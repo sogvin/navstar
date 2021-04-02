91ff1178f3fbdd4636391890636ce36b0a6a8d1a
Gregory Vincic <g@7de.se>
2021-04-02 10:13:49 +0200

    Move user

---
+++ user.go
@@ -0,0 +1,25 @@
+package navstar
+
+// User implements domain logic for operations on the System and
+// should be used through a role.
+type User struct {
+	sys *System
+}
+
+func (me *User) Use(v *System, role Role) Role {
+	me.sys = v
+	role.setUser(me)
+	return role
+}
+
+// Keep all feature methods private and expose only through roles
+
+func (me *User) submitFlightplan(route Route) error {
+	// implement...
+	return nil
+}
+
+func (me *User) listRoutes() ([]Route, error) {
+	// implement list routes
+	return nil, nil
+}
